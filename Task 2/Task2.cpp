7007a423931535306e0a5952f5b710dffce80467
Nikita <nikitastrel4@gmail.com>
2022-05-20 19:16:14 +0400

    Add tasks

---
+++ Task 2/Task2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <locale>
+#include <fstream>
+
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    float a, b, c; 
+    float x, y;
+    ifstream fin("input1.txt");
+
+    fin >> a;
+    fin >> b;
+    fin >> c;
+
+    ifstream fin2("input2.txt");
+
+    while (!fin2.eof())
+    {
+
+        fin2 >> x;
+        fin2 >> y;
+
+        if ((x - a)*2 + (y - b) == c*2 )
+        {
+            cout << 0 << endl;
+        }
+
+        if ((x - a) * 2 + (y - b) < c * 2)
+        {
+            cout << 1 << endl;
+        }
+
+        if ((x - a) * 2 + (y - b) > c * 2)
+        {
+            cout << 2 << endl;
+        }
+    }
+    
+
+}
+
---
+++ Task 4/Task4.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ifstream fin("input.txt" );
+	vector<int> nums;
+	int sum = 0;
+	int result;
+	int count = 0;
+
+	while (!fin.eof())
+	{
+		int a;
+		fin >> a;
+		nums.push_back(a);
+	}
+
+	sort(nums.begin(), nums.end());
+
+	for (int i = 0; i < nums.size(); i++)
+	{
+		sum += nums[i];
+	}
+
+	result = sum / nums.size();
+
+	for (int i = 0; i < nums.size(); i++)
+	{
+		while (nums[i] != result)
+		{
+			if (nums[i] > result)
+			{
+				nums[i] -= 1;
+				count++;
+			}
+
+			if (nums[i] < result)
+			{
+				nums[i] += 1;
+				count++;
+			}
+			
+		}
+	}
+
+	cout << count;
+}
+
+
